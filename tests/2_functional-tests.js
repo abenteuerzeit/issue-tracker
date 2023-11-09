7f3f75f350e60b9d3d4b7aa2fbd19979ee21eb52
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-11-09 22:34:58 +0000

    refactor: flatten tests and simplify

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,135 +1,80 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
-const assert = chai.assert;
+const { assert } = chai;
 const server = require('../server');
 
 chai.use(chaiHttp);
+
 suite('Functional Tests', () => {
-
     suite('/api/issues/:project', () => {
-
-        suite('GET', () => {
-
-            suite('Reading and Issue', () => {
-
-                suite('200 OK', () => {
-
-                    test('returns all issues array with all fields', (done) => {
-                        assert.fail('GET request not implemented yet');
-                        done();
-                    });
-
-                    test('filters issues by any single query parameter', (done) => {
-                        assert.fail('GET request not implemented yet');
-                        done();
-                    });
-
-                    test('filters issues by multiple query parameters', (done) => {
-                        assert.fail('GET request not implemented yet');
-                        done();
-                    });
-                })
-            });
+        test('GET returns all issues with all fields', (done) => {
+            assert.fail('GET request not implemented yet');
+            done();
         });
 
-        suite('POST', () => {
+        test('GET filters issues by a single query parameter', (done) => {
+            assert.fail('GET request not implemented yet');
+            done();
+        });
 
-            suite('Creating an issue', () => {
+        test('GET filters issues by multiple query parameters', (done) => {
+            assert.fail('GET request not implemented yet');
+            done();
+        });
 
-                suite('201 Created', () => {
+        test('POST creates an issue with required fields', (done) => {
+            assert.fail('POST request not implemented yet');
+            done();
+        });
 
-                    suite('Required fields provided (issue_title, issue_text, created_by)', () => {
+        test('POST creates an issue with optional fields defaulted', (done) => {
+            assert.fail('POST request not implemented yet');
+            done();
+        });
 
-                        test('Optional fields provided', (done) => {
-                            assert.fail('POST request not implemented yet');
-                            done();
-                        });
+        test('POST returns an error when required fields are missing', (done) => {
+            assert.fail('POST request not implemented yet');
+            done();
+        });
 
-                        test('Optional fields empty when not provided. (created_on (date/time), updated_on (date/time), open (boolean, true for open - default value, false for closed), and _id)', (done) => {
-                            assert.fail('POST request not implemented yet');
-                            done();
-                        });
-                    })
-                });
+        test('PUT updates issue fields and updated_on timestamp when a single field is updated', (done) => {
+            assert.fail('PUT request not implemented yet');
+            done();
+        });
 
-                suite('400 Bad Request', () => {
+        test('PUT updates issue fields and updated_on timestamp when multiple fields are updated', (done) => {
+            assert.fail('PUT request not implemented yet');
+            done();
+        });
 
-                    test('returns { error: "required field(s) missing" } when required fields are missing ', (done) => {
-                        assert.fail('POST request not implemented yet');
-                        done();
-                    });
-                });
-            });
+        test('PUT returns an error when _id is missing', (done) => {
+            assert.fail('PUT request not implemented yet');
+            done();
+        });
 
-            suite('PUT', () => {
+        test('PUT returns an error when no update fields are provided', (done) => {
+            assert.fail('PUT request not implemented yet');
+            done();
+        });
 
-                suite('Updating issue by _id', () => {
+        test('PUT returns an error when _id is invalid', (done) => {
+            assert.fail('PUT request not implemented yet');
+            done();
+        });
 
-                    suite('200 OK', () => {
+        test('DELETE successfully deletes an issue with a valid _id', (done) => {
+            assert.fail('DELETE request not implemented yet');
+            done();
+        });
 
-                        suite('returns {  result: "successfully updated", "_id": _id }', () => {
+        test('DELETE returns an error when _id is invalid', (done) => {
+            assert.fail('DELETE request not implemented yet');
+            done();
+        });
 
-                            suite('Updates the updated_on field', () => {
-
-                                test('when one field is updated', (done) => {
-                                    assert.fail('PUT request not implemented yet');
-                                    done();
-                                });
-
-                                test('when many fields are updated', (done) => {
-                                    assert.fail('PUT request not implemented yet');
-                                    done();
-                                });
-                            });
-                        });
-                    });
-
-                    suite('400 Bad Request', () => {
-
-                        test('returns  { error: "missing _id" } when _id is missing', (done) => {
-                            assert.fail('PUT request not implemented yet');
-                            done();
-                        });
-
-                        test('returns { error: "no update field(s) sent", "_id": _id } when no update fields are provided', (done) => {
-                            assert.fail('PUT request not implemented yet');
-                            done();
-                        });
-
-                        test('returns { error: "could not update", "_id": _id } when _id is invalid', (done) => {
-                            assert.fail('PUT request not implemented yet');
-                            done();
-                        });
-                    });
-                });
-            });
-
-            suite('DELETE', () => {
-
-                suite('Deleting an issue by _id', () => {
-
-                    suite('204 No Content', () => {
-
-                        test('returns { result: "successfully deleted", "_id": _id } on successful deletion', (done) => {
-                            assert.fail('DELETE request not implemented yet');
-                            done();
-                        });
-                    });
-
-                    suite('400 Bad Request', () => {
-
-                        test('returns { error: "could not delete", "_id": _id } when _id is invalid', (done) => {
-                            assert.fail('DELETE request not implemented yet');
-                            done();
-                        });
-
-                        test('returns { error: "missing _id" } when _id is missing', (done) => {
-                            assert.fail('DELETE request not implemented yet');
-                            done();
-                        });
-                    });
-                });
-            });
+        test('DELETE returns an error when _id is missing', (done) => {
+            assert.fail('DELETE request not implemented yet');
+            done();
         });
     });
 });
