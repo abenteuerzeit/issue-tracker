7dab8f95b7501525ce0a5628313bf4774e624c1e
Adrian Mróz, Ph.D. <dr.adrian.mroz@outlook.com>
2023-11-13 12:04:01 +0100

    test: all tests passing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -81,6 +81,66 @@ suite("/api/issues/:project", function () {
       });
   });
 
+  // Tests any combination of query parameters
+  //
+  // const projectFields = [
+  //   "_id", "issue_title", "issue_text", "created_by", "assigned_to", "status_text", "created_on", "updated_on", "open"
+  // ];
+  
+  // function getCombinations(array, size) {
+  //   function* doCombination(offset, combo) {
+  //     if (combo.length === size) {
+  //       yield combo;
+  //       return;
+  //     }
+  //     for (let i = offset; i < array.length; i++) {
+  //       yield* doCombination(i + 1, combo.concat(array[i]));
+  //     }
+  //   }
+  //   return Array.from(doCombination(0, []));
+  // }
+  
+  // for (let size = 1; size <= projectFields.length; size++) {
+  //   getCombinations(projectFields, size).forEach(combination => {
+  //     test(`GET filters issues by ${combination.join(", ")}`, (done) => {
+  //       let parameters = {};
+  
+  //       combination.forEach(key => {
+  //         switch (key) {
+  //           case "_id":
+  //             parameters[key] = "5871dda29faedc3491ff93bb";
+  //             break;
+  //           case "issue_title":
+  //           case "issue_text":
+  //           case "created_by":
+  //           case "assigned_to":
+  //           case "status_text":
+  //             parameters[key] = "Joe";
+  //             break;
+  //           case "created_on":
+  //           case "updated_on":
+  //             parameters[key] = "2017-01-08T06:35:14.240Z";
+  //             break;
+  //           case "open":
+  //             parameters[key] = true;
+  //             break;
+  //           default:
+  //             parameters[key] = "";
+  //         }
+  //       });
+  
+  //       chai
+  //         .request(server)
+  //         .get(endpoint)
+  //         .query(parameters)
+  //         .end((err, res) => {
+  //           assert.equal(res.status, httpsResponse.ok);
+  //           done();
+  //         });
+  //     });
+  //   });
+  // }
+
   test("POST creates an issue with required fields", (done) => {
     let newIssue = {
       issue_title: "new issue test",
@@ -569,7 +629,6 @@ suite("/api/issues/:project", function () {
                 .type("form")
                 .send(deleteIssue)
                 .end((err, res) => {
-                    console.log(res.body);
                     assert.equal(
                         res.status,
                         httpsResponse.badRequest,
